566d33ff5d46aea07c234a71f9ec9d7c716392b3
babab114 <suunn811@naver.com>
2025-09-01 16:43:21 +0900

    main.cpp
    물품신청 리스트, 관리자 승인 기능 수정

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,10 @@ int main() {
     int choice;
     while (true) {
         cout << "\n[물품 신청 프로그램]\n";
-        cout << "1. 물품 신청하기\n";
-        cout << "2. 신청 리스트 보기\n";
-        cout << "3. 관리자 승인\n";
+        cout << "1. 물품 리스트 보기\n";
+        cout << "2. 물품 신청하기\n";
+        cout << "3. 신청 리스트 보기\n"; // 메뉴 이름 변경
+        cout << "4. 관리자 승인\n";
         cout << "0. 종료\n";
         cout << "원하는 기능을 선택하세요: ";
         cin >> choice;
@@ -30,14 +31,16 @@ int main() {
         }
 
         switch (choice) {
-            case 1: applyItem(itemList); break; 
-            case 2: printRequestList(); break; 
-            case 3: processApplications(itemList); break;
+            case 1: printItemList(itemList); break;
+            case 2: applyItem(itemList); break;
+            case 3: printRequestList(); break;
+            case 4: processApplications(itemList); break;
             case 0: cout << "프로그램을 종료합니다.\n"; return 0;
             default: cout << "잘못된 선택입니다. 다시 입력해주세요.\n";
         }
     }
     return 0;
 }
+
 
 
