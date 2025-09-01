ab0c516141294ac8b8de40cc3d6b86a9fce33496
babab114 <suunn811@naver.com>
2025-09-01 16:21:09 +0900

    2일차 입력오류처리보완 - main
    2일차 입력오류처리보완

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,20 @@ int main() {
         cout << "0. 종료\n";
         cout << "원하는 기능을 선택하세요: ";
         cin >> choice;
+        
+        // 입력 오류 처리 (숫자 입력 검사)
+        if (cin.fail()) {
+            cout << "오류: 잘못된 입력입니다. 숫자를 입력해주세요.\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            choice = -1; // default case 로 이동
+        }
 
         switch (choice) {
             case 1: applyItem(itemList); break; 
             case 2: printRequestList(); break; 
             case 0: cout << "프로그램을 종료합니다.\n"; return 0;
+            default: cout << "잘못된 선택입니다. 다시 입력해주세요.\n";
         }
     }
     return 0;
